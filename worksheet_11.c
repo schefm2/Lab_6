19255dfa122ee1701affd5599965c6e3f5b802d7
Tom Saad <tomsaad5739@gmail.com>
2017-12-07 14:14:24 -0500

    spacing

--- worksheet_11.c
+++ worksheet_11.c
@@ -37,24 +37,31 @@ void main()
     Sys_Init();
     putchar(' ');
     error = desired-actual;
+
 //  two byte calculation
     temp_motorpw_2byte = pw_neut+kp*error+kd*(error-previous_error);
     printf("The two byte calculation of motorpw is %d \r\n",temp_motorpw_2byte);
+
 //  equation form 1, long ints
     temp_motorpw_alg1 = pw_neut+kp*error+kd*(error-previous_error);
     printf("Algorithm 1, four byte calculation of motorpw is %ld \r\n",temp_motorpw_alg1);
+
 //  equation form 2, long ints
     temp_motorpw_alg2 = pw_neut+(signed long)kp*error+kd*(error-previous_error);
     printf("Algorithm 2, four byte calculation of motorpw is %ld \r\n",temp_motorpw_alg2);
+
 //  equation form 3, long ints
     temp_motorpw_alg3 = (signed long)( pw_neut + kp * (error) + kd * (error - previous_error));
     printf("Algorithm 3, four byte calculation of motorpw is %ld \r\n",temp_motorpw_alg3);
+
 //  equation form 4, long ints
     temp_motorpw_alg4 = pw_neut+kp*(signed int)(error)+kd*(signed int)(error-previous_error);
     printf("Algorithm 4, four byte calculation of motorpw is %ld \r\n",temp_motorpw_alg4);
+
 //  equation form 5, long ints
     temp_motorpw_alg5 = (signed long)pw_neut+(signed long)(kp*(error))+(signed long)(kd*(error-previous_error));
     printf("Algorithm 5, four byte calculation of motorpw is %ld \r\n",temp_motorpw_alg5);
+
 //  equation form 6, long ints
     temp_motorpw_alg6 = (signed long)pw_neut+(signed long)kp*(signed long)error+(signed long)kd*(signed long)(error-previous_error);
     printf("Algorithm 6, four byte calculation of motorpw is %ld \r\n",temp_motorpw_alg6);
