d68853ed774b66feee450b4ee7adf3806915533d
Matthew Scheffer <schefm2@rpi.edu>
2017-12-10 17:33:46 -0500

    Normalizes pulse widths for motors as per instruction of grading rubric

--- Data/graphing.m
+++ Data/graphing.m
@@ -2,8 +2,8 @@ current_data = allthedata07;
 time = .1*(1:size(current_data,1)); %coefficient reflects
                                     %each data point is 100 ms
 
-current_data(:,6) = (current_data(:,6) - 2027)/(3502 - 2027)*100;
-current_data(:,7) = (current_data(:,7) - 2425)/(3245 - 2425)*100;
+current_data(:,6) = (current_data(:,6) - 2765)/(3502 - 2027)*200;
+current_data(:,7) = (current_data(:,7) - 2835)/(3245 - 2425)*200;
 
 close all
 figure(1)
@@ -16,7 +16,7 @@ plot(time,current_data(:,1),'--k')
 plot(time,current_data(:,2), ':k')
 plot([.1 .1*size(current_data,1)],[0 0], '-k', 'LineWidth',.5)
 xlabel('Time [s]')
-ylabel('Normalized Percentage, 0 to 100')
+ylabel('Tenths of a Degree')
 % xlim([35 50]) %useful for looking in detail at ranger run
 legend('Heading Error', 'Desired Heading','Current Heading', 'Location', 'southeast')
 
@@ -25,7 +25,7 @@ hold on
 plot(time,current_data(:,6),'-k','LineWidth',1.5)
 plot(time,current_data(:,7),'x-k','MarkerIndices',(1:25:size(current_data,1)))
 xlabel('Time [s]')
-ylabel('Pulse Width')
+ylabel('Normalized Percentage, -100 to 100')
 legend('Motor PW','Servo PW','Location','southeast')
 
 %----------------------------------------------------------------
