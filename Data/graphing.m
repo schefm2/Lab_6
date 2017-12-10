0b560648b22b5edb914b99147f10177f5af45216
Matthew Scheffer <schefm2@rpi.edu>
2017-12-10 17:08:44 -0500

    Adds data that are ready for figure creation and analysis

--- Data/graphing.m
+++ Data/graphing.m
@@ -1,4 +1,4 @@
-current_data = finaldata001;
+current_data = allthedata15;
 time = .1*(1:size(current_data,1)); %coefficient reflects
                                     %each data point is 100 ms
 
@@ -10,44 +10,52 @@ subplot(2,1,1)
 hold on
 plot(time,current_data(:,3),'-k','LineWidth',1.5)
 plot(time,current_data(:,1),'--k')
-plot([0 32],[0 0], '-.k')
-legend('Heading Error', 'Desired Heading', 'Location', 'southeast')
+plot(time,current_data(:,2), ':k')
+plot([.1 .1*size(current_data,1)],[0 0], '-k', 'LineWidth',.5)
+xlabel('Time [s]')
+ylabel('Unitless Magnitude')
+% xlim([35 50]) %useful for looking in detail at ranger run
+legend('Heading Error', 'Desired Heading','Current Heading', 'Location', 'southeast')
 
 subplot(2,1,2)
 hold on
 plot(time,current_data(:,6),'-k','LineWidth',1.5)
 plot(time,current_data(:,7),'x-k','MarkerIndices',(1:25:size(current_data,1)))
+xlabel('Time [s]')
+ylabel('Pulse Width')
 legend('Motor PW','Servo PW','Location','southeast')
 
 %----------------------------------------------------------------
-figure(2)
-set(gcf,'Visible','on') %Automatically pop out the figure
-clf
-
-subplot(2,1,1)
-plot(time,current_data(:,5),'-k')
-legend('Battery Voltage','Location','southeast')
-
-subplot(2,1,2)
-plot(time,current_data(:,4),'-k')
-legend('Range','Location','southeast')
-ylim([0 300])
+% figure(2)
+% set(gcf,'Visible','on') %Automatically pop out the figure
+% clf
+% 
+% subplot(2,1,1)
+% plot(time,current_data(:,5),'-k')
+% xlabel('Time [s]')
+% ylabel('Battery Voltage [mV]')
+% 
+% subplot(2,1,2)
+% plot(time,current_data(:,4),'-k')
+% xlabel('Time [s]')
+% ylabel('Range [cm]')
+% ylim([0 300])
 
 %----------------------------------------------------------------
 %For the data with the fans turned off and derivative gain set to 0
-figure(3)
-set(gcf,'Visible','on')
-clf
-
-subplot(2,1,1)
-hold on
-plot(time,current_data(:,2),'-k',time,current_data(:,1),'--k')
-plot(time,current_data(:,3),'x-.k','MarkerIndices',(1:25:size(current_data,1)))
-legend('Actual Heading','Desired Heading','Heading Error',...
-    'Location','southeast')
-
-subplot(2,1,2)
-plot(time,current_data(:,6),'-k')
-legend('Motor PW')
-xlabel('Time [s]')
+% figure(3)
+% set(gcf,'Visible','on')
+% clf
+% 
+% subplot(2,1,1)
+% hold on
+% plot(time,current_data(:,2),'-k',time,current_data(:,1),'--k')
+% plot(time,current_data(:,3),'x-.k','MarkerIndices',(1:25:size(current_data,1)))
+% legend('Actual Heading','Desired Heading','Heading Error',...
+%     'Location','southeast')
+% 
+% subplot(2,1,2)
+% plot(time,current_data(:,6),'-k')
+% legend('Motor PW')
+% xlabel('Time [s]')
 
