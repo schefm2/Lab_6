73f8438e32389447e1eed58d26f66a688ea10c1f
Matthew Scheffer <schefm2@rpi.edu>
2017-12-11 15:20:53 -0500

    Adds a smoother ranger run at optimal settings

--- Data/graphing.m
+++ Data/graphing.m
@@ -1,4 +1,4 @@
-current_data = allthedata07;
+current_data = allthedata17;
 time = .1*(1:size(current_data,1)); %coefficient reflects
                                     %each data point is 100 ms
 
@@ -11,9 +11,9 @@ set(gcf,'Visible','on') %Automatically pop out the figure
 clf
 subplot(2,1,1)
 hold on
-plot(time,current_data(:,3),'-k','LineWidth',1.5)
+plot(time,current_data(:,3),':k')
 plot(time,current_data(:,1),'--k')
-plot(time,current_data(:,2), ':k')
+plot(time,current_data(:,2),'-k','LineWidth',1.5)
 plot([.1 .1*size(current_data,1)],[0 0], '-k', 'LineWidth',.5)
 xlabel('Time [s]')
 ylabel('Tenths of a Degree')
@@ -29,36 +29,37 @@ ylabel('Normalized Percentage, -100 to 100')
 legend('Motor PW','Servo PW','Location','southeast')
 
 %----------------------------------------------------------------
-% figure(2)
-% set(gcf,'Visible','on') %Automatically pop out the figure
-% clf
-% 
-% subplot(2,1,1)
-% plot(time,current_data(:,5),'-k')
-% xlabel('Time [s]')
-% ylabel('Battery Voltage [mV]')
-% 
-% subplot(2,1,2)
-% plot(time,current_data(:,4),'-k')
-% xlabel('Time [s]')
-% ylabel('Range [cm]')
-% ylim([0 300])
+figure(2)
+set(gcf,'Visible','on') %Automatically pop out the figure
+clf
+
+subplot(2,1,1)
+plot(time,current_data(:,5),'-k')
+xlabel('Time [s]')
+ylabel('Battery Voltage [mV]')
+
+subplot(2,1,2)
+plot(time,current_data(:,4),'-k')
+xlabel('Time [s]')
+ylabel('Range [cm]')
+ylim([0 300])
 
 %----------------------------------------------------------------
 %For the data with the fans turned off and derivative gain set to 0
-% figure(3)
-% set(gcf,'Visible','on')
-% clf
-% 
-% subplot(2,1,1)
-% hold on
-% plot(time,current_data(:,2),'-k',time,current_data(:,1),'--k')
-% plot(time,current_data(:,3),'x-.k','MarkerIndices',(1:25:size(current_data,1)))
-% legend('Actual Heading','Desired Heading','Heading Error',...
-%     'Location','southeast')
-% 
-% subplot(2,1,2)
-% plot(time,current_data(:,6),'-k')
-% legend('Motor PW')
-% xlabel('Time [s]')
+figure(3)
+set(gcf,'Visible','on')
+clf
+
+subplot(2,1,1)
+hold on
+plot(time,current_data(:,6),'-k')
+legend('Motor PW','Location','southeast')
+xlabel('Time [s]')
+ylabel('Normalized Percentage, -100 to 100')
+
+subplot(2,1,2)
+plot(time,current_data(:,5),'-k')
+legend('Battery Voltage')
+xlabel('Time [s]')
+ylabel('Voltage [mV]')
 
